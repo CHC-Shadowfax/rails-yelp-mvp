93e81b420b18fddd66c2771ec88cf77bdc4d5fea
Cesar Huallpa <c.huallpa@alumni.ipade.mx>
2023-06-08 18:47:29 -0600

    pendiente una validación de número

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
+  REVIEW_SCORE = [0, 1, 2, 3, 4, 5]
   belongs_to :restaurant
+  validates :content, presence: true
+  validates :rating, inclusion: { in: REVIEW_SCORE }
 end
