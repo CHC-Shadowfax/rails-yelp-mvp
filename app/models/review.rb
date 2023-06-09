6c38ee0e637e14bdb8c68e46b576d46196935d9c
Cesar Huallpa <c.huallpa@alumni.ipade.mx>
2023-06-08 18:57:56 -0600

    added numericalitty validation for review rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
-  REVIEW_SCORE = [0, 1, 2, 3, 4, 5]
+  # REVIEW_SCORE = 0..5
   belongs_to :restaurant
   validates :content, presence: true
-  validates :rating, inclusion: { in: REVIEW_SCORE }
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: 0..5 }
 end
