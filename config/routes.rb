7aaa5828b6fe41ebdb3dffe371efb408d9f0ddbf
Cesar Huallpa <c.huallpa@alumni.ipade.mx>
2023-06-09 22:00:05 -0600

    typo corrected

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :restaurants do
     resources :reviews, only: [:new, :create]
   end
+  resources :reviews, only: [:destroy] # you dont need the restaurant to destroy a review
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
